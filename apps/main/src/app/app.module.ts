f47f4ce31241b0c2559a028c006494665168cf24
Richard Reinicke <richard.reinicke@ahk-freiburg.de>
2022-03-28 10:11:42 +0200

    added esri assets to app

--- apps/main/src/app/app.module.ts
+++ apps/main/src/app/app.module.ts
@@ -4,9 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
+import { MappingModule } from '@ahk-freiburg/hwrm-mapping';
+
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    MappingModule.forRoot({
+      assetPath: './assets/esri'
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
